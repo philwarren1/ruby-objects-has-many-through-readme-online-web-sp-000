61442673687ea215a8d1ca4f857eb285ed380573
Philip Warren <philwarren1@gmail.com>
2020-08-13 15:20:59 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -22,8 +22,9 @@ attr_accessor :name, :yrs_experience
     meal.waiter == self #checking for waiter now
   end
  end 
-  def best_tipper
-  best_tipped_meal = meals.max do |meal_a, meal_b|
+ 
+ def best_tipper
+   best_tipped_meal = meals.max do |meal_a, meal_b|
     meal_a.tip <=> meal_b.tip
   end
  end
